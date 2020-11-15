476fe00b50909b025cb720ac883f2365a575bc2d
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2020-11-15 20:53:57 +0000

    prisma

---
+++ hackernews-node/prisma/cmd.sh
@@ -0,0 +1,10 @@
+npm install @prisma/cli --save-dev
+
+mkdir prisma
+touch prisma/schema.prisma
+
+npx prisma migrate save --experimental
+
+npx prisma migrate up --experimental
+
+npx prisma generate
