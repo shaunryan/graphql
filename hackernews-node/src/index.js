2e81e23a07fc13284a02bf342ae06cb9120eea91
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2020-11-15 16:01:06 +0000

    init

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,18 +1,40 @@
 const { graphQLServer, GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-    info: String!
-}`
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+// 1
+let idCount = links.length
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => {
+        const link = links.find(link => link.id === args.id)
+        return link
     }
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
+
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
